876d44bec655eda228cf9ce12f56e6a6d61c05d8
Phot Koseekrainiramon <kosee.phot@gmail.com>
2024-07-28 06:14:02 +1000

    add: optimizeserver load by caching with apicache

--- packages/api-gateway/index.ts
+++ packages/api-gateway/index.ts
@@ -1,11 +1,14 @@
 import express, { Application } from "express";
+import apicache from "apicache";
 import apiRoutes from "./routes";
 
 const app: Application = express();
+const cache = apicache.middleware;
 const port = 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cache("3 minutes"));
 
 app.get("/", (req, res) => res.send("LINE MAN Wongnai Frontend Assignment"));
 app.use("/api", apiRoutes);
