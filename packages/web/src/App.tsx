e0476bc29e4feb6ec33adfe75d5d0ec81e08bb6b
Phot Koseekrainiramon <kosee.phot@gmail.com>
2024-07-28 20:18:27 +1000

    init tailwind css + react-router-dom

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,10 +1,13 @@
+import "./index.css";
 
 function App() {
   return (
     <div>
-      <h1>Edit this app to complete LINE MAN Wongnai Frontend Assignment!</h1>
+      <h1 className="text-3xl">
+        Edit this app to complete LINE MAN Wongnai Frontend Assignment!
+      </h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Restuarant from "./pages/Restuarant";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/restaurant/:restaurantId/",
+    element: <Restuarant />,
+  },
+]);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
